ea7e4afe51e84aa2a9f7201849764a1888a5f679
Sumanth Akkala <sumanth.akkala@gmail.com>
2020-06-14 16:38:39 -0400

    implemented search functionality

--- iNotesPro/Controllers/NotesTableViewTableViewController.swift
+++ iNotesPro/Controllers/NotesTableViewTableViewController.swift
@@ -11,19 +11,28 @@ import CoreData
 
 class NotesTableViewTableViewController: UITableViewController {
     
-    var notes = [Note]()
+    var originalNotesDataSource = [Note]()
+    var currentNotesDataSource = [Note]()
     var clickedNote: Note? = nil
     var clickedNoteIndex: Int? = nil
 
+    @IBOutlet weak var searchContainerView: UIView!
+    var searchController: UISearchController!
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(reloadTableView(_:)), name: NotificationConstants.noteCreated, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(reloadTableView(_:)), name: NotificationConstants.noteUpdated, object: nil)
         
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchContainerView.addSubview(searchController.searchBar)
+        searchController.searchBar.delegate = self
     
         let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
         do{
-            self.notes = try PersistanceService.context.fetch(fetchRequest)
+            self.originalNotesDataSource = try PersistanceService.context.fetch(fetchRequest)
+            self.currentNotesDataSource = self.originalNotesDataSource
             self.tableView.reloadData()
         } catch{
             
@@ -38,19 +47,20 @@ class NotesTableViewTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes.count
+        return currentNotesDataSource.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "activeNotesTableCell") as! NotesTableViewCell
 
-        cell.noteCellTitle?.text = notes[indexPath.row].noteTitle
-        cell.noteCellDescription?.text = notes[indexPath.row].noteDescription
+        cell.noteCellTitle?.text = currentNotesDataSource[indexPath.row].noteTitle
+        cell.noteCellDescription?.text = currentNotesDataSource[indexPath.row].noteDescription
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        clickedNote = notes[indexPath.row]
+        clickedNote = currentNotesDataSource[indexPath.row]
         clickedNoteIndex = indexPath.row
+        searchController.isActive = false
         self.performSegue(withIdentifier: "activeNoteCellClicked", sender: self)
 
 
@@ -59,16 +69,23 @@ class NotesTableViewTableViewController: UITableViewController {
     
     @objc func reloadTableView(_ notification: Notification){
         //load data here
-        if let data = notification.userInfo as? [String: Note]
+        if notification.name == NotificationConstants.noteCreated
         {
-            for (_, note) in data
+            let data = notification.userInfo as? [String: Note]
+            for (_, note) in data!
             {
-                notes.append(note)
+                currentNotesDataSource = originalNotesDataSource
+                originalNotesDataSource.append(note)
+                currentNotesDataSource.append(note)
             }
         }
-        if let data = notification.userInfo as? [String: Any]
+        if notification.name == NotificationConstants.noteUpdated
         {
-            notes[data["index"] as! Int] = data["data"] as! Note
+            let data = notification.userInfo as? [String: Any]
+            let indexInOriginalDataSource = originalNotesDataSource.firstIndex(of: clickedNote!)
+            currentNotesDataSource = originalNotesDataSource
+            currentNotesDataSource[indexInOriginalDataSource!] = data!["data"] as! Note
+            //currentNotesDataSource[data!["index"] as! Int] = data!["data"] as! Note
         }
         self.tableView.reloadData()
     }
@@ -81,5 +98,48 @@ class NotesTableViewTableViewController: UITableViewController {
             destination?.noteData = self.clickedNote
             destination?.noteIndex = self.clickedNoteIndex
         }
+    }
+    
+    func filterCurrentDataSource(searchTerm: String) {
+        if searchTerm.count > 0 {
+            //currentNotesDataSource = originalNotesDataSource
+            let filteredNotes = currentNotesDataSource.filter{ ($0.noteTitle?.replacingOccurrences(of: " ", with: "").lowercased().contains(searchTerm.replacingOccurrences(of: " ", with: "").lowercased()))!
+                ||
+                ($0.noteDescription?.replacingOccurrences(of: " ", with: "").lowercased().contains(searchTerm.replacingOccurrences(of: " ", with: "").lowercased()))!
+            }
+            currentNotesDataSource = filteredNotes
+            self.tableView.reloadData()
+        }
+    }
+    func restoreCurrentDataSource(){
+        currentNotesDataSource = originalNotesDataSource
+        self.tableView.reloadData()
+    }
+}
+
+
+extension NotesTableViewTableViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filterCurrentDataSource(searchTerm: searchText)
+        }
+    }
+    
+    
+}
+
+extension NotesTableViewTableViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchController.isActive = false
+        if let searchText = searchBar.text {
+            filterCurrentDataSource(searchTerm: searchText)
+        }
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchController.isActive = false
+//        if let searchText = searchBar.text, !searchText.isEmpty {
+//            restoreCurrentDataSource()
+//        }
+        restoreCurrentDataSource()
     }
 }
